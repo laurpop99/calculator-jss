79950a1e93274fd3e1d9ca90f75fb17e94cfd81a
Popescu Laurentiu <laurpop99@yahoo.com>
2021-10-13 17:21:28 +0300

    Added project files

---
+++ script.js
@@ -0,0 +1,19 @@
+function operate(operator, num1, num2){
+    return operator(num1,num2);
+}
+
+function addNumbers(num1,num2){
+    return num1 + num2;
+}
+
+function subtractNumbers(num1, num2){
+    return num1 - num2;
+}
+
+function multiplyNumbers(num1, num2){
+    return num1 * num2;
+}
+
+function divideNumbers(num1, num2){
+    return +(num1 / num2);
+}
